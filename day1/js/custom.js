41c05f2ca2d865377e094fc6c6f18ed9e8849d33
Prabin268 <shresthaprabin540@gmail.com>
2024-12-19 12:05:41 +0545

    first commit

---
+++ day1/js/custom.js
@@ -0,0 +1,55 @@
+"use strict";
+
+// username = 'ram'
+// console.log(username)
+
+// var -> global scope
+// let -> block scope
+// const -> unchangeable
+
+// for(var x=1; x<=5; x++){
+//     console.log(x)
+// }
+
+// console.log(x)
+
+// const x=10; 
+// x=20; 
+// console.log(x)
+
+// let x=10; 
+// console.log(x)
+
+// user_name: snake case -> variable
+// UserName: pascal case -> class oop
+// userName: camel case -> function
+
+// class Myconsole{
+//     x=10
+
+//     info(){
+//         console.log('info')
+//     }
+// }
+
+// let obj = new Myconsole()
+
+// console.log(console)
+
+// console.log(window)
+
+// window.username = 'ram'
+
+// console.log(username)
+
+// let username
+
+// username = 'ram'
+// console.log(username)
+
+// console.warn('This is warning')
+
+// let data=['ram','shyam','hari']
+// console.table(data)
+
+console.log('Hello world')
---
+++ js/demo.js
@@ -0,0 +1,46 @@
+// let option = prompt("1. Dell(Rs20000) \n 2. Lenovo(Rs30000) \n 3. HP(Rs 50000)")
+// if (option  == 1){
+//   let quantity = prompt ("enter the quantity")
+//   let total = 20000 * quantity
+//   let product_name = "Dell"
+//   alert("total amount is" +  total)
+// }
+// else if (option == 2){
+//   let quantity = prompt ("enter the quantity")
+//   let total = 30000 * quantity
+//   let product_name = "Lenovo"
+//   alert("total amount is" +  total)
+// }
+// else if (option == 3){
+//   let quantity = prompt ("enter the quantity")
+//   let total = 50000 * quantity
+//   let product_name = "HP"
+//   alert("total amount is" +  total)
+// }
+// else{
+//   alert("invalid option")
+// }
+
+// let distane = prompt("ringroad distance = 27km")
+// let option = prompt("1.Sadhdobato-Gwarko \n 2.Koteshwor-Baneshwor \n 3.Kalanki-Balkhu")
+// if (option == 1) 
+// {
+//     let cost=15
+//     alert("total cost is" + cost)
+// }
+// else if (option == 2)
+//     {
+//     let cost=30
+//     alert("total cost is" + cost)
+// }
+// else if (option == 3)
+//     {
+//     let cost=45
+//     alert("total cost is" + cost)
+// }
+// else{
+//     alert("invalid option")
+// }
+
+// for,while,do while,nested,for in,for of
+// MDN
